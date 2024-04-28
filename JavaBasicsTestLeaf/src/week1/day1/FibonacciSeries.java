f4002a5291a817d17da37d81bbf4f9e3c5305fda
Anitha <anitharaghavan0794@gmail.com>
2024-04-28 08:04:26 +0530

    TestLeafAssignments Day1

---
+++ JavaBasicsTestLeaf/src/week1/day1/FibonacciSeries.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n = 8, firstTerm = 0, secondTerm = 1;
+	    System.out.println("Fibonacci Series till " + n + " terms:");
+
+	    for (int i = 1; i <= n; i++) {
+	      System.out.print(firstTerm + ", ");
+
+	      // compute the next term
+	      int nextTerm = firstTerm + secondTerm;
+	      firstTerm = secondTerm;
+	      secondTerm = nextTerm;
+	    }
+	}
+
+}
